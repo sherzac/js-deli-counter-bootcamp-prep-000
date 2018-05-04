9e5a772922f6d2e9400ea728399f986215290251
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-04 13:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeliLine, name) {
+/*function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return 'Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.';
-}
+}*/
