6461722c04976aca5426833297e9ff36b249ad54
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-04 14:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(x) {
   if (x.length === 0) {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
+  } else {
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
   }
 }
