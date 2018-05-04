1bcc27268be15d37f9f29c5621072cb7de241aee
Shaun Rezac <shaun.rezac@gmail.com>
2018-05-04 14:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function nowServing(x) {
     return "Currently serving " + name + ".";
   }
 }
+
+
+function currentLine(x) {
+  var line = [];
+  if (x.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0, i < x.length; i++) {
+      line += (i + 1) + ". " + x[i] +", "
+    }
+    line = line.slice(0 line.length-2)
+    return 
+  }
+}
